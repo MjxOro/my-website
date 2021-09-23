66c0558afbf489720df855021f4a6d6ef1aa941f
Matthew Oro <MatthewEOro@gmail.com>
2021-09-23 13:08:58 -0400

    fix contacts component responsiveness

--- src/components/Contact/Contact.tsx
+++ src/components/Contact/Contact.tsx
@@ -12,13 +12,13 @@ export const Contacts: React.FC<any> = () => {
       <group>
         <Html
           className="contact"
-          position={mobile ? [-1, 4, 0] : window.innerWidth < 1280 ? [-1, 7, 0] : window.innerWidth < 1400 ? [-1.35, 7, 0] : [-1.7, 7, 0]}
+          position={mobile ? [-1, 4, 0] : window.innerWidth < 1280 ? [-1, 7, 0] : window.innerWidth < 1400 ? [-1.35, 7, 0] : [-1.7, 5, 0]}
         >
           <h1 className='contact__title'>Contacts</h1>
         </Html>
         <Html
           className="contact"
-          position={mobile ? [-1.65, 2, 0] : window.innerWidth < 1280 ? [-2.25, 4, 0] : [-3, 5, 0]}
+          position={mobile ? [-1.65, 2, 0] : window.innerWidth < 1280 ? [-2.25, 4, 0] : [-3, 3, 0]}
         >
           <p className='contact__text'>Let's stay connected</p>
         </Html>
